e70b952171222d8aa62a105441f06688d3656629
EltonFull9 <43542168+EltonFull9@users.noreply.github.com>
2022-03-29 09:23:48 -0300

    adicionei os arquivos da calculadora

---
+++ script.js
@@ -0,0 +1,12 @@
+var valor1 = 2
+var valor2 = 5
+var valor3 = 7
+var valor4 = 3
+
+valor1 = (valor1 + valor2 +valor3 + valor4) / 4
+
+
+if(valor1 >= 6)
+  console.log("Aprovado", valor1)
+  else
+    console.log("Reprovado", valor1)
